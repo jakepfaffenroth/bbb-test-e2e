ef1c3edf14ad4edd36cfbd67a32e72f5d16b0ab5
Jake Pfaffenroth <jake.p@wompmobile.com>
2022-05-04 12:27:05 -0700

    Added teardown script path config

--- configs/paths.js
+++ configs/paths.js
@@ -1,5 +1,6 @@
 module.exports = {
   globalSetupDir: "setup/global-setup.js",
+  globalTeardownDir: "setup/global-teardown.js",
   testDir: "tests",
   outputReportDir: "test-results/test-report",
   outputDir: "test-results/test-results",
